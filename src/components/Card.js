427ca42eeaae2c46d441e952e9e4aebaaa1fc635
vo1eriyah <dream.on.813@gmail.com>
2019-10-27 18:29:33 +0600

    5th/decomposing components

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Main.css'
+
+function  Card (props){
+    return (
+        <div 
+        className="My-card">
+            <h3>{props.cardTitle}</h3>
+                    <ul>
+                        <li>I'm proud of nothing</li>
+                        <li>I love dogs</li>
+                        <li>I hate unconcious people</li>
+                    </ul>
+        </div>
+    )
+}
+
+export default Card;
