66b7eab49507c92d43871c09f98e25938bcf9e87
vo1eriyah <dream.on.813@gmail.com>
2019-10-27 18:36:13 +0600

    6th/decomposing components it#2

--- src/components/Self-info.js
+++ src/components/Self-info.js
@@ -5,9 +5,8 @@ function Selfinfo(props){
     return (
         <div 
         className="My-info">
-            <br/>
-            <h2></h2>
-            <h1>👋🏻Hello, my name is {props.name}!</h1>
+            <h1>👋🏻</h1>
+            <h1>Hello, my name is {props.name}!</h1>
             <p>I'm a very stupid woman with a pink dreams🌈
             </p>                
             <Card cardTitle="This are 3 things about me:"/>
