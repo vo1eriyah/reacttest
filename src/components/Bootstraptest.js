6a7f388ab21270b887a831de777f024466fcf7e5
vo1eriyah <dream.on.813@gmail.com>
2019-11-12 19:27:40 +0600

    11th

---
+++ src/components/Bootstraptest.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Bootstraptest() {
+    return(
+       
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                One of three columns
+                <div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
+                </div>
+                <div class="col-sm">
+                One of three columns
+                </div>
+                <div class="col-sm">
+                One of three columns
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Bootstraptest
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -5,8 +7,9 @@ import * as serviceWorker from './serviceWorker';
 import Footer from './components/Footer';
 import CurrentDate from './components/CurrentDate'
 import './components/Main.css'
+import Bootstraptest from './components/Bootstraptest'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Bootstraptest />, document.getElementById('root'));
 ReactDOM.render(<CurrentDate />, document.getElementById("datetime"));
 
 serviceWorker.unregister();
