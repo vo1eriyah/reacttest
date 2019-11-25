ba6bff3d68cc94893b2ac87cc3981ee97eded789
vo1eriyah <dream.on.813@gmail.com>
2019-11-25 21:04:14 +0600

    flex -- hide block

--- src/components/AppFlex.js
+++ src/components/AppFlex.js
@@ -4,31 +4,44 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 function AppFlex() {
     return(
         <div class='container'>
-            <Grid
+            <Grid 
             />
                 <Row>
                     <Col xs={6}>
-                        <div id="col">col1</div>
+                        <div class="col">col1</div>
                     </Col>
 
                     <Col xs={6}>
-                        <div id="col">col2</div>
+                        <div class="col">col2</div>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs={2}>
-                        <div id="col">col1</div>
+                        <div class="col">col1</div>
                     </Col>
 
                     <Col xs={6}>
-                        <div id="col">col2</div>
+                        <div class="col">col2</div>
                     </Col>
 
                     <Col md={2}>
-                        <div id="col">col3</div>
+                        <div class="col">col3</div>
                     </Col>
                     <Col lg={2}>
-                        <div id="col">col4</div>
+                        <div class="col">col4</div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={3}>
+                        <div class="col">col1</div>
+                    </Col>
+
+                    <Col md={6}>
+                        <div class="col">col2</div>
+                    </Col>
+                    <Col lg={3}>
+                        <div class="col todelete">col to delete</div>
                     </Col>
                 </Row>
             </div>
