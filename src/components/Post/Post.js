8c0765796ad326d832e5ae367440541ceaa190c0
vo1eriyah <dream.on.813@gmail.com>
2019-12-09 09:29:26 +0600

    post class

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,34 +2,42 @@ import React from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Avatar from '../Profile/Avatar';
 
-function Post (){
-    return(
-        <div >
-            <Grid className="post-container">
-                <Row className="post_auth_desc">
-                    <Col className="author_photo"><Avatar width="32" height="32"/></Col>
-                    <Col className="author_info">
-                        <Row lg={12}><span className="author_name">Mathew <i className="author_nickname">@catchmcconaughey</i></span></Row>
-                        <Row lg={12}>
-                            <Col className="permalink"><a className="link" href="#">Jl. Pantai Kuta</a></Col>                     
-                            <Col className="post_date">12.12.2019</Col>
-                        </Row>
-                    </Col>
-                </Row>
 
-                <Row className="post_content" >
-                    <Col lg={12} xs={12}>
-                    <p>
-                    The Venom symbiote requires a perfect match to bond with its host; otherwise, the host is doomed. The Venom movie plays like a midnight horror that has invaded a superhero movie, and while there are some laffs to be had, it's far from a perfect match. 
-                    <a className="link">#venommovie</a>
+class Post extends React.Component{
+    constructor (post){
+        super(post)
+    }
 
-                    <img className="post_photo" src="https://st.kp.yandex.net/im/kadr/3/2/6/kinopoisk.ru-Venom-3260983.jpg"></img>
-                    </p>
-                    </Col>
-                </Row>
-            </Grid>
-        </div>
-    );
+    render (props) {
+        return (
+            <div >
+                <Grid className="post-container">
+                    <Row className="post_auth_desc">
+                        <Col className="author_photo"><Avatar width="32" height="32"/></Col>
+                        <Col className="author_info">
+                            <Row lg={12}><span className="author_name">{this.props.author_name} <i className="author_nickname">{this.props.author_nickname}</i></span></Row>
+                            <Row lg={12}>
+                                <Col className="location"><a className="link" href="#">Jl. Pantai Kuta</a></Col>                     
+                                <Col className="post_date">12.12.2019</Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
+                    <Row className="post_content" >
+                        <Col lg={12} xs={12}>
+                        <p>
+                        The Venom symbiote requires a perfect match to bond with its host; otherwise, the host is doomed. The Venom movie plays like a midnight horror that has invaded a superhero movie, and while there are some laffs to be had, it's far from a perfect match. 
+                        <a className="link">#venommovie</a>
+
+                        <div className="post_photo_solo"><img src="https://st.kp.yandex.net/im/kadr/3/2/6/kinopoisk.ru-Venom-3260983.jpg"></img></div>
+                        </p>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+
 }
 
 export default Post
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,7 +10,7 @@ class SelfInfo extends React.Component{
         return(
             <Grid direction="column" spacing={8}>
                     <Row flexGrow={1} flexBasis='auto' horizontal='space-between'>
-                        <Col className="avatar" lg={2} sm={3} xs={2}>
+                        <Col className="avatar" lg={2} sm={3} xs={2} md={3}>
                             <Avatar width="48px" height="48px" imgurl="https://scontent.fiev1-1.fna.fbcdn.net/v/t1.0-9/34179438_1798769306858268_5254876844385107968_o.jpg?_nc_cat=106&_nc_oc=AQlkiFUEdv7suFUNZFZxZZ6fMN8w-2LSRZhDmcnZ41lT8e3s5XU7w86wgmy5b9eREKA&_nc_ht=scontent.fiev1-1.fna&oh=b525227803f5e3b2dcb8a383e0a89c21&oe=5E5661CD"/>
                         </Col>
                         <Col className="self-info" xs={9} lg={9}>
