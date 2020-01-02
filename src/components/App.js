08c47fe756a840e809d80fec3617a2d91b54ad43
vo1eriyah <dream.on.813@gmail.com>
2020-01-02 13:40:03 +0600

    grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,19 @@
 import React from 'react'
-import { Grid, Row, Col } from 'react-flexbox-grid';
+//import { Grid, Row, Col } from 'react-flexbox-grid';
 import Footer from './Footer/Footer';
 import Profile from './Profile/Profile';
 import Post from './Post/Post';
 import Menu from './Menu/Menu';
 import MainButton from './Buttons/MainButton';
 import MobileMenu from './Menu/MobileMenu';
+import MyGrid from './MyGrid';
+
 
 function App() {
     return(
-       
-       <div className='my_container'>
-                <div className="mobile_menu" ><MobileMenu/></div>
-
-                    <div className="col_third">
-                        <aside>
-                            <Profile/>
-                            <Menu/>
-                            <MainButton caption="Write post!"/>
-                        </aside>
-                        </div>
-                        <div className="col_half">
-                            <Post author_name="Mathew" author_nickname="@catchmccounaghey"/>
-                        </div> 
-                        <div className="col_third">                 
-                            <div className="wallet" ></div>
-                        </div>
-                    
-           </div>
+       <div >
+           <MyGrid/>
+       </div>
     );
 }
 
---
+++ src/components/MyGrid.js
@@ -0,0 +1,159 @@
+import React from 'react'
+
+function MyGrid() {
+    return(
+       <div className="wrapper">
+           <div class="Grid Grid--full">
+                <div class="Grid-cell">
+                    <div class="Demo content-1of1">1</div>
+                </div>
+            </div>
+
+            <div className="Grid Grid--gutters Grid--cols-2">
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of2">1/2</div>
+                    </div>   
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of2">1/2</div>
+                    </div>        
+            </div>
+            
+            <div className="Grid Grid--gutters Grid--cols-3">
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of3">1/3</div>
+                    </div>   
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of3">1/3</div>
+                    </div>      
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of3">1/3</div>
+                    </div>       
+            </div>
+
+            <div className="Grid Grid--gutters Grid--cols-4">
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of4">1/4</div>
+                    </div>   
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of4">1/4</div>
+                    </div>      
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of4">1/4</div>
+                    </div>       
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of4">1/4</div>
+                    </div>       
+            </div>
+
+            <div className="Grid Grid--gutters Grid--cols-6">
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of6">1/6</div>
+                    </div>   
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of6">1/6</div>
+                    </div>      
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of6">1/6</div>
+                    </div>       
+
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of6">1/6</div>
+                    </div>       
+                    <div className="Grid-cell">
+                        <div className="Demo content-1of6">1/6</div>
+                    </div>    
+            </div>
+
+            <div className="Grid Grid--gutters Grid--cols-12">
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+
+                <div className="Grid-cell">
+                    <div className="Demo content-1of12">1/12</div>
+                </div>
+            </div>
+
+            <div className="Grid Grid--gutters Grid--1of3">
+            <div className="Grid-cell"><div className="Demo content-1of3">1/3</div></div>  
+            <div className="Grid-cell"><div className="Demo">auto</div>
+            </div>
+        </div>
+
+        <div class="Grid Grid--gutters Grid--1of3 Grid--nested">
+    <div class="Grid-cell"><div class="Demo content-1of3">1/3</div></div>
+    <div class="Grid-cell"><div class="Demo">     
+      <div class="Grid Grid--gutters Grid--1of3 Grid--nested">
+        <div class="Grid-cell"><div class="Demo content-1of3">1/3</div></div>
+        <div class="Grid-cell"><div class="Demo">
+          <div class="Grid Grid--gutters Grid--1of3 Grid--nested">
+            <div class="Grid-cell"><div class="Demo content-1of3">1/3</div></div>
+            <div class="Grid-cell"><div class="Demo">auto</div></div>
+          </div>        
+        </div></div>
+      </div>      
+    </div>
+    </div>
+  </div>
+        </div>
+
+        
+        
+
+        
+    );
+}
+
+export default MyGrid;
