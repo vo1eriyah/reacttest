5c74c59adaa81f68d443585da2844309407a5caf
vo1eriyah <dream.on.813@gmail.com>
2020-01-04 12:49:06 +0600

    --svg as components

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -49,9 +49,8 @@ class Post extends React.Component{
                             <div className="post_photo_solo"><img src={this.props.photourl}></img></div>
                         </p>
 
-                        <ShareBlock btcLikeCount={this.props.btcLikeCount} liked={this.props.liked} commentCount={this.props.commentCount} repostCount={this.props.repostCount}/>
+                        <ShareBlock btcLikeCount={this.props.btcLikeCount} liked={this.props.liked} commented={this.props.commented} reposted={this.props.commented} commentCount={this.props.commentCount} repostCount={this.props.repostCount}/>
                     </div>
-
 
                 </div>
             </div>
--- src/components/Post/ShareBlock.js
+++ src/components/Post/ShareBlock.js
@@ -1,34 +1,39 @@
 import React from 'react'
-import BTClikeIcn from './BTClike.svg'
-import CommentIcn from './comment.svg'
-import RepostCountIcn from './repost.svg'
 import './post.scss';
+import BTClikeIcon from './BTCLikeIcon'
+import CommentIcon from './CommentIcon'
+import RepostIcon from './RepostIcon'
 
 class ShareBlock extends React.Component {
     constructor(ShareBlock){
         super(ShareBlock)
         this.state = {
-            likeiconstate: this.props.liked,
-            commenticonstate: this.props.commented,
-            repostediconstate: this.props.reposted
+            liked: this.props.liked,
+            commented: this.props.commented,
+            reposted: this.props.reposted
         }
     }
 
     render(props){
+        let listitem =[];
 
-        if (this.state.likeiconstate===true){
-            document.getElementById('btc-icon').classList.add('liked')
-        }
+        (this.state.liked == 1) ? listitem.push(<div className="like"><span className="icon liked"><BTClikeIcon /></span><span className="count liked">{this.props.btcLikeCount}</span></div>) 
+        : listitem.push(<div className="like"><span className="icon"><BTClikeIcon/></span><span className="count">{this.props.btcLikeCount}</span></div>);
+        
+        (this.state.commented == 1) ? listitem.push(<div className="like"><span className="icon liked"><CommentIcon/></span><span className="count liked">{this.props.commentCount}</span></div>)
+        : listitem.push(<div className="like"><span id="comment-icon" className="icon"><CommentIcon/></span><span className="count">{this.props.commentCount}</span></div>);
+
+        (this.state.reposted == 1) ? listitem.push(<div className="like"><span className="icon liked"><RepostIcon/></span><span className="count liked">{this.props.repostCount}</span></div>)
+        : listitem.push(<div className="like"><span className="icon"><RepostIcon/></span><span className="count">{this.props.repostCount}</span></div>)
+        
+        
 
         return(
             <React.Fragment>
-            <div className="divider"><hr/></div>
-            <div className="ShareBlock">
-
-                <div className="like"><span className="icon"><img id="btc-icon" src={BTClikeIcn}/></span><span className="count liked">{this.state.likeiconstate}{this.props.btcLikeCount}</span></div>
-                <div className="like"><span className="icon"><img src={CommentIcn}/></span><span className="count">{this.props.commentCount}</span></div>
-                <div className="like"><span className="icon"><img src={RepostCountIcn}/></span><span className="count">{this.props.repostCount}</span></div>
-            </div>
+                <div className="divider"><hr/></div>
+                <div className="ShareBlock">
+                    {listitem}
+                </div>
             </React.Fragment>
         );
     }
